68e5fe6c689c93e3880390d00f259078e343eb77
Tiago Mendes <mendestiago@ua.pt>
2025-05-14 09:43:41 +0100

    fixed AI response images
    ( ° ͜ʖ °)

--- js/ciii-questionsAI.js
+++ js/ciii-questionsAI.js
@@ -10,10 +10,10 @@ function handleMessage() {
         interactionDiv.innerHTML = `
             <div style="display: flex; align-items: right; justify-content: flex-end; margin-right: 20%;">
                 <div class="speech-bubble-right" style="top: 25%;  margin-right: 2%;">Can you please explain what the gradient vector represents in multivariable calculus?</div>
-                <img src="../assets/icons/user.png" style="position: relative;" width="50" class="rounded-circle img-fluid">
+                <img src="../assets/icons/user.png" style="position: relative;" width="50" height="50" class="rounded-circle">
             </div>
             <div style="display: flex; align-items: left; margin-left: 5%; margin-top: 1%;">
-                <img src="../assets/icons/pixel.png" style="position: relative;" width="50" class="rounded-circle img-fluid">
+                <img src="../assets/icons/icon1coloredCIII.png" style="position: relative;" width="50" height="50" class="rounded-circle">
                 <div class="speech-bubble-left" style="top: 25%; margin-left: 2%;">The gradient vector represents the direction and rate of the steepest increase of a scalar function. It points in the direction where the function increases most rapidly and its magnitude corresponds to the rate of change in that direction.</div>
             </div>
             `;
--- js/generalAI.js
+++ js/generalAI.js
@@ -10,10 +10,10 @@ function handleMessage() {
         interactionDiv.innerHTML = `
             <div style="display: flex; align-items: right; justify-content: flex-end; margin-right: 20%;">
                 <div class="speech-bubble-right" style="top: 25%;  margin-right: 2%;">What are the best times to study?</div>
-                <img src="../assets/icons/user.png" style="position: relative;" width="50" height="50" class="rounded-circle img-fluid">
+                <img src="../assets/icons/user.png" style="position: relative;" width="50" height="50" class="rounded-circle">
             </div>
             <div style="display: flex; align-items: left; margin-left: 5%; margin-top: 1%;">
-                <img src="../assets/icons/pixel.png" style="position: relative;" width="50" class="rounded-circle img-fluid">
+                <img src="../assets/icons/icon1colored.png" style="position: relative;" width="50" height="50" class="rounded-circle">
                 <div class="speech-bubble-left" style="top: 25%; margin-left: 2%;">The best times to study are usually when you feel most alert and focused. For many people, this is in the morning after a good night's sleep, or in the early evening when energy levels are still stable. The key is to find a consistent time that fits your personal routine and avoid distractions during that period.</div>
             </div>
             `;
