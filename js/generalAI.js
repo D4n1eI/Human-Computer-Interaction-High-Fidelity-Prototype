e9033be870eb1f13bae7bf05c95c2a2c76075e2e
Daniel <dan.andrade@ua.pt>
2025-05-14 21:30:34 +0100

    Add - A few easter eggs

--- js/generalAI.js
+++ js/generalAI.js
@@ -42,7 +42,87 @@ function handleMessage() {
         defaultHeader.style.display = "none";
 
         messageInput.value = "";
+    }else if (message === "who is the most precious and handsome boy?") {
+        interactionDiv.innerHTML = `
+            <div style="display: flex; align-items: right; justify-content: flex-end; margin-right: 20%;">
+                <div class="speech-bubble-right" style="top: 25%;  margin-right: 2%;">who is the most precious and handsome boy?</div>
+                <img src="../assets/icons/user.png" style="position: relative;" width="50" height="50" class="rounded-circle">
+            </div>
+            <div style="display: flex; align-items: left; margin-left: 5%; margin-top: 1%;">
+                <img src="../assets/icons/icon1colored.png" style="position: relative;" width="50" height="50" class="rounded-circle">
+                <div class="speech-bubble-left" style="top: 25%; margin-left: 2%;">
+                    <img src="../assets/pixel/mybeloved2.jpeg" alt="Pixel" >
+                    <br><br>
+                    <p>Our beloved Pixel boy, of course!</p>
+                </div>
+            </div>
+            `;
+
+        defaultHeader.style.display = "none";
+
+        messageInput.value = "";
+    }else if (message === "who is pixel?") {
+        interactionDiv.innerHTML = `
+            <div style="display: flex; align-items: right; justify-content: flex-end; margin-right: 20%;">
+                <div class="speech-bubble-right" style="top: 25%;  margin-right: 2%;">who is pixel?</div>
+                <img src="../assets/icons/user.png" style="position: relative;" width="50" height="50" class="rounded-circle">
+            </div>
+            <div style="display: flex; align-items: left; margin-left: 5%; margin-top: 1%;">
+                <img src="../assets/icons/icon1colored.png" style="position: relative;" width="50" height="50" class="rounded-circle">
+                <div class="speech-bubble-left" style="top: 25%; margin-left: 2%;">
+                    <img src="../assets/pixel/mybeloved3.jpeg" alt="Pixel" >
+                    <br>A very handsome boy!<br>
+                    <p></p>
+                </div>
+            </div>
+            `;
+
+        defaultHeader.style.display = "none";
+
+        messageInput.value = "";
     }
+
+    else if (message === "who is riscas?") {
+    interactionDiv.innerHTML = `
+        <div style="display: flex; align-items: right; justify-content: flex-end; margin-right: 20%;">
+            <div class="speech-bubble-right" style="top: 25%;  margin-right: 2%;">who is riscas?</div>
+            <img src="../assets/icons/user.png" style="position: relative;" width="50" height="50" class="rounded-circle">
+        </div>
+        <div style="display: flex; align-items: left; margin-left: 5%; margin-top: 1%;">
+            <img src="../assets/icons/icon1colored.png" style="position: relative;" width="50" height="50" class="rounded-circle">
+            <div class="speech-bubble-left" style="top: 25%; margin-left: 2%;">
+                <img src="../assets/pixel/naughtyboy.jpeg" alt="Pixel" style="width: 120px; height: auto;">
+                <br>A very naughty boy! He craves the chickens!!OHNOMNOMNOMNOMNOM<br>
+                <p></p>
+            </div>
+        </div>
+        `;
+
+    defaultHeader.style.display = "none";
+    messageInput.value = "";
+}
+
+ else if (message === "why is riscas a naughty boy?") {
+    interactionDiv.innerHTML = `
+        <div style="display: flex; align-items: right; justify-content: flex-end; margin-right: 20%;">
+            <div class="speech-bubble-right" style="top: 25%;  margin-right: 2%;">why is riscas a naughty boy?</div>
+            <img src="../assets/icons/user.png" style="position: relative;" width="50" height="50" class="rounded-circle">
+        </div>
+        <div style="display: flex; align-items: left; margin-left: 5%; margin-top: 1%;">
+            <img src="../assets/icons/icon1colored.png" style="position: relative;" width="50" height="50" class="rounded-circle">
+            <div class="speech-bubble-left" style="top: 25%; margin-left: 2%;">
+                <img src="../assets/pixel/naughtyboycancelhim.jpeg" alt="Pixel" style="width: 120px; height: auto;">
+                <br>He tried to grow catnip and sell it on the black market, the silly goober!<br>
+                <p></p>
+            </div>
+        </div>
+        `;
+
+    defaultHeader.style.display = "none";
+    messageInput.value = "";
+}
+
+    
 }
 
 sendButton.addEventListener("click", handleMessage);
